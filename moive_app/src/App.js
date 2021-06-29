6f0cb1ddc34856d2bd33d9eb7f912f7a59a55b13
miraejang <mirae611@nate.com>
2021-06-29 14:00:09 +0900

    use array map method in app

--- moive_app/src/App.js
+++ moive_app/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-function Food({fav}) {
-  console.log(fav)
-  return <h3>I Like {fav}</h3>;
+function Food({name, picture}) {
+  return <div>
+    <h3>I Like {name}</h3>
+    <img src={picture} alt={name} />
+  </div>
 }
+
+const foodILike = [
+  {
+    name: 'pizza',
+    image: 'https://bigseventravel.com/wp-content/uploads/2019/11/paulie-1024x727.png',
+  }, {
+    name: 'coffee',
+    image: 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-1240w.jpg',
+  }, {
+    name: 'cake',
+    image: 'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/rainbow_cake_20402_16x9.jpg',
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Food fav="pizza" />
-      <Food fav="coffee" />
-      <Food fav="cake" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   )
 }
