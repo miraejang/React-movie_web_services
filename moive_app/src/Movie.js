a40bddb61b52a4f8457e00d7f5372790a56eb764
miraejang <mirae611@nate.com>
2021-07-01 11:24:32 +0900

    add css

--- moive_app/src/Movie.js
+++ moive_app/src/Movie.js
@@ -4,15 +4,15 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className="movies__movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
                 <p className="movie__summary">{summary}</p>
-                <ul className ="genres">
+                <ul className ="movie__genres">
                     {genres.map((genre, index) => 
-                        <li key="index" className="genres__genre">{genre}</li>
+                        <li key={index} className="genres__genre">{genre}</li>
                     )}
                 </ul>
             </div>
