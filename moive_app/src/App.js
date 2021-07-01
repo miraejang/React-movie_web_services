09ed1f84641c7fabe915374b003d7f18c8d44d95
miraejang <mirae611@nate.com>
2021-07-01 23:40:17 +0900

    completed router

--- moive_app/src/App.js
+++ moive_app/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-    </HashRouter>
+      <Route path="/movie/:id" component={Detail} />
+    </BrowserRouter>
   )
 }
 export default App;
---
+++ moive_app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+function Navigation() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </nav>
+    )
+}
+
+export default Navigation;
